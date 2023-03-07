b4f5c6c7855296a8b11b0ba5590f0b1c06c60e43
Marilena-C <marilena.cancel@gmail.com>
2023-03-07 22:52:42 +0000

    remove the quote that was typedby mistake  in generateMarkdown js, replaced input wit type in questions array and change npm ver to 8.2.4

---
+++ README-file.md
@@ -0,0 +1,26 @@
+# asdsa
+  ##Table of contents:
+    1. [Description] (#Description)
+    2. [Installation] (#Installation)
+    3. [Usage](#Usage)  
+    4. [Contributing](#Contributing)
+    5. [Tests](#Tests)
+    6. [Licence](#Licence)
+    7. [GitHub](#GitHub)
+    8. [E-mail](#E-mail)
+## Description
+asd 
+## Installation
+asd
+## Usage
+asd
+## Contributing
+asd
+## Tests
+undefined
+## Licence
+Apache License 2.0
+## GitHub
+asd
+## E-mail
+asd
--- index.js
+++ index.js
@@ -1,102 +1,79 @@
-import inquirer from "inquirer";
+//import inquirer from "inquirer";
+//add the modules that are needed for this project
 //import fs from 'fs/promises'
 const fs = require("fs");
 const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
+
 // array of questions for user
 const questions = [
     {
+        name: 'title',
+        type: 'input',
+        message: "What is your project title?"
+    },
+    {
         name: 'description',
-        input: 'input',
-        message: "What is your file description?"
+        type: 'input',
+        message: "What is your project description?"
     },
     {
         name: 'installation',
-        input: 'input',
-        message: "What are your installation instructions?"
+        type: 'input',
+        message: "What are the installation instructions for your application?"
     },
     {
         name: 'usage',
-        input: 'input',
-        message: "Tell us about the usage of your app:"
-    },
-    {
-        name: 'github',
-        input: 'input',
-        message: "What is your github profile?"
+        type: 'input',
+        message: "How do you use your application?"
     },
     {
         name: 'contributing',
-        input: 'input',
-        message: "Would you like to contribute to this app?"
-    }
+        type: 'input',
+        message: "How can users contribute to this project?"
+    },
+    {
+        name: 'tests',
+        type: 'input',
+        message: "How can users update the tests for your project?"
+    },
+    {
+        name: 'licence',
+        type: 'list',
+        message: 'What licence do you use for this repository?',
+        choices: ["Apache License 2.0", "GNU General Public License 3.0", "MIT", "None"]
+    },
+    {
+        name: 'github',
+        type: 'input',
+        message: "What is your github username?"
+    },
+    {
+        name: 'email',
+        type: 'input',
+        message: "What is your email address where users can get in touch with questions and suggestions?"
+    },
+
 ];
 
-const {description, installation, usage, github, contributing} = await inquirer
-    .prompt([
-        {
-            name: 'description',
-            input: 'input',
-            message: "What is your file description?"
-        },
-        {
-            name: 'installation',
-            input: 'input',
-            message: "What are your installation instructions?"
-        },
-        {
-            name: 'usage',
-            input: 'input',
-            message: "Tell us about the usage of your app:"
-        },
-        {
-            name: 'github',
-            input: 'input',
-            message: "What is your github profile?"
-        },
-        {
-            name: 'contributing',
-            input: 'input',
-            message: "Would you like to contribute to this app?"
-        }
-])
+// console.log(title, description, installation, usage, test, github, email, contributing);
 
-console.log(description, installation, usage, github, contributing);
 // function to write README file
-function writeToFile(description, installation, usage, github, contributing) {
-}const md = `
-<!DOCTYPE md>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mini_Project</title>
-</head>
-<body>
-    <h1>${description}</h1>
-    <h2>To install this app you need to follow these instructions</h2>
-    <p>${installation}</p>
-    <h2>usage</h2>
-    <p>${usage}</p>
-    <h3>Check out my github: https://github.com/${github}</h3>
-    <h3>Please do not hesitate to add your suggestions here if you want to contribute: https://github.com/Marilena-C/CmdL-README-Generator/${contributing}</h3>
-    
-</body>
-</md>
-`
-
-
-// function to initialize program
-function init() {
-  
+function writeToFile(fileName, data) {
+    fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
-// function call to initialize program
-init(await fs.writeFile('README.md', md));
+//function to initialize program
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((answers) => {
+            console.log(answers);
+            writeToFile("README-file.md", generateMarkdown(answers));
+        });
+}
 
-
-//await fs.writeFile('README.md', md)
-//await fs.writeFile('index.html', html)
+// function call to to callback - wait for user to answer in Terminal and then run next Action 
+init();
